e336b78e2ae7a7d9b0994c7dc960e155999c5d00
Malte Kiefer <malte.kiefer@intellytec.de>
2023-08-02 04:22:24 +0200

    first commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,73 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -gx EDITOR nvim
+set -gx FZF_CTRL_T_COMMAND nvim
+
+
+# git prompt settings
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_showdirtystate 'yes'
+set -g __fish_git_prompt_char_stateseparator ' '
+set -g __fish_git_prompt_char_dirtystate "✖"
+set -g __fish_git_prompt_char_cleanstate "✔"
+set -g __fish_git_prompt_char_untrackedfiles "…"
+set -g __fish_git_prompt_char_stagedstate "●"
+set -g __fish_git_prompt_char_conflictedstate "+"
+set -g __fish_git_prompt_color_dirtystate yellow
+set -g __fish_git_prompt_color_cleanstate green --bold
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_branch cyan --dim --italics
+
+# brew install jump, https://github.com/gsamokovarov/jump
+status --is-interactive; and source (jump shell fish | psub)
+
+set fish_greeting
+
+alias vim='nvim'
+
+alias ls='exa -al --color=always --group-directories-first' # my preferred listing
+alias la='exa -a --color=always --group-directories-first'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first'  # long format
+alias lt='exa -aT --color=always --group-directories-first' # tree listin
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# confirm before overwriting something
+alias cp="cp -i"
+alias mv='mv -i'
+alias rm='rm -i'
+
+# adding flags
+alias df='df -h'                          # human-readable sizes
+alias free='free -m'                      # show sizes in MB
+
+# yt-dlp
+alias yta-aac="yt-dlp --extract-audio --audio-format aac "
+alias yta-best="yt-dlp --extract-audio --audio-format best "
+alias yta-flac="yt-dlp --extract-audio --audio-format flac "
+alias yta-m4a="yt-dlp --extract-audio --audio-format m4a "
+alias yta-mp3="yt-dlp --extract-audio --audio-format mp3 "
+alias yta-opus="yt-dlp --extract-audio --audio-format opus "
+alias yta-vorbis="yt-dlp --extract-audio --audio-format vorbis "
+alias yta-wav="yt-dlp --extract-audio --audio-format wav "
+alias ytv-best="yt-dlp -f bestvideo+bestaudio "
+
+# the terminal rickroll
+alias rr='curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash'
+
+# bare git repo alias for dotfiles
+abbr --add --global  config "/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+
+# termbin
+alias tb="nc termbin.com 9999"
+
+colorscript -ex jangofett
+
+### SETTING THE STARSHIP PROMPT ###
+starship init fish | source
+
